4addb24a1dc6ff6bb393e48faf3f34e5b156f354
adrianyang2012 <69127002+adrianyang2012@users.noreply.github.com>
2021-04-16 11:34:42 -0700

    Create read_the_dictinary.py
    created at 4/16/2021
    11:33 
    ------
    first edited
    -----
    output
    "print means output something with its value(s)

---
+++ read_the_dictinary.py
@@ -0,0 +1,8 @@
+import make_dictinary
+#example
+#for key,value in dictinary_name:
+#  print(f"{key} means {value.title()} ")
+example = make_dictinary(["print"],["output something with its value(s)"])
+for key, value in example:
+  print(f"{key} means {value.title()}")
+            
