693d2eeef76c9b9f36967e29d569117e97090513
adrianyang2012 <69127002+adrianyang2012@users.noreply.github.com>
2021-04-21 11:29:27 -0700

    Update read_the_dictinary.py
    added what not to do

--- read_the_dictinary.py
+++ read_the_dictinary.py
@@ -5,4 +5,5 @@ import make_dictinary
 example = make_dictinary(["print"],["output something with its value(s)"])
 for key, value in example:
   print(f"{key} means {value.title()}")
-            
+what_happends = make_dictinary([make_dicinary(["#"],["comment to note something",make_dictinary["//"],["comment to note something"])
+print("do not do this")                                                     
